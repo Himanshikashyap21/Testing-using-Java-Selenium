6f902f486cc539b443a6a278a8c6d9865dae5a93
Himanshikashyap21 <himanshikashyap541@gmail.com>
2026-02-17 12:51:53 +0530

    Java

---
+++ javaLearning/src/basicDay1/Test.java
@@ -0,0 +1,9 @@
+package basicDay1;
+
+public class Test {
+	
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println("Hello Future SDET Himanshi :)");
+}
+}
---
+++ javaLearning/src/basicDay1/childprogram.java
@@ -0,0 +1,5 @@
+package basicDay1;
+
+public class childprogram {
+	
+}
